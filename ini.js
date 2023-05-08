7d12e37ddd8e1536c5a1bccf36b736be5d4e2850
Omar Hernandez <dycoo@msn.com>
2023-05-08 12:59:57 -0600

    line for v1.0.2

--- ini.js
+++ ini.js
@@ -1,7 +1,12 @@
 //this is the fist line for Git
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 console.log('line for V.1.0.0');
 =======
 console.log('line for V.1.0.1');
 >>>>>>> ef68606 (update)
+=======
+console.log('V1.0.1');
+console.log('V1.0.2');
+>>>>>>> 21905e2 (line for v1.0.2)
